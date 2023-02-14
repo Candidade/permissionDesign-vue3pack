d23efb6ca0c7695023c517eeb878898c9fa8274d
Candi <candiem@qq.com>
2023-02-14 15:37:31 +0800

    'init'

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia';
+export const useGlobalStore = defineStore('main', {
+  state: () => ({ count: 0 }),
+  getters: {
+    double: (state) => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+  },
+});
